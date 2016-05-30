318a313bd135432056cf7366518dfe6418a95a54
Ralph Das <ralphdas@MBP-van-Ralph-2.localdomain>
2016-05-30 14:35:05 +0200

    mongoose uitleg

--- index.js
+++ index.js
@@ -1,10 +1,50 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser')
 
-app.get('/', function(req, res){
-  res.json({
-  	text: 'Hallo World Again!'
-  });
+var user_schema = new mongoose.Schema({
+	first_name: String,
+	last_name: String,
+	age: Number  	
 });
+
+app.use(bodyParser.json({strict:false}));
+
+mongoose.connect('mongodb://localhost/whyfollow');
+
+var user = mongoose.model('user', user_schema);
+
+app.get('/users', function(_req, _resp){
+  
+  	user.find({}, function(err, data){
+  		
+  		_resp.send(data);
+
+		_resp.end();
+
+  	});
+
+
+  
+});
+
+
+app.post('/users/', function(req, resp){
+	
+
+	
+	var new_user = user.create({
+		first_name: req.body.first_name,
+		last_name: req.body.last_name,
+		age: req.body.age,
+
+	});
+	
+	resp.writeHead(200);
+	resp.end();
+});
+
+
 
 app.listen(8080);
